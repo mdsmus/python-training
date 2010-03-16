d758d60bda5de520b5bf8d295159151bf4e343b6
Pedro Kroger <pedro.kroger@gmail.com>
2010-03-16 10:41:52 -0300

    mais coisas

--- exemplo1.py
+++ exemplo1.py
@@ -5,11 +5,14 @@
 
 notas = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
 
+
 def transpose(lista_de_notas, eixo):
     return [(x + eixo) % 12 for x in lista_de_notas]
+
 
 def show(lista_de_numeros):
     return [notas[x] for x in lista_de_numeros]
 
-print transpose([1,20,3], 3)
-print show(transpose([1,20,3], 3))
+
+print transpose([1, 20, 3], 3)
+print show(transpose([1, 20, 3], 3))
